3a5de83c7946f3507bf17b8e31477bbff6f13b66
Alex <ssaasshha11@yandex.ru>
2023-06-26 13:28:16 +0400

    make facade for savers

---
+++ AppealManager.Presentation/Utilities/RKKStatsSaverFacade.cs
@@ -0,0 +1,26 @@
+﻿using AppealManager.Application.Models;
+
+namespace AppealManager.Presentation.Utilities
+{
+    public class RKKStatsSaverFacade
+    {
+        public string SaveStatsAs(IEnumerable<RKKStats> stats, SaveOptions saveOptions)
+        {
+            var saver = GetRKKSaver(saveOptions);
+            return saver.SaveStats(stats);
+        }
+
+        public IRKKStatsSaver GetRKKSaver(SaveOptions saveOptions)
+            => saveOptions switch
+            {
+                SaveOptions.HTML_TABLE => new RKKStatsSaverAsHtml(),
+                SaveOptions.PLAIN_TEXT => new RKKStatsSaverAsPlainText(),
+            };
+
+        public enum SaveOptions
+        {
+            HTML_TABLE,
+            PLAIN_TEXT
+        }
+    }
+}
