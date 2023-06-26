33830692c7d55a0e52249b1839343a311898a9de
Alex <ssaasshha11@yandex.ru>
2023-06-26 13:26:56 +0400

    add service that read content of sended file

---
+++ AppealManager.Presentation/Services/TextFileReader.cs
@@ -0,0 +1,34 @@
+﻿using LanguageExt;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace AppealManager.Presentation.Services
+{
+    public class TextFileReader
+    {
+        private readonly string fileType = "text/plain";
+        private readonly long allowedSize = 512 * 1024;
+
+        /// <summary>
+        /// Читает файл
+        /// </summary>
+        /// <param name="file">Файл</param>
+        /// <returns>
+        /// Возвращает Some(text), если тип файла text/plain,
+        /// иначе None
+        /// </returns>
+        public async Task<Option<string>> ReadFileAsync(IBrowserFile file)
+        {
+            if (file.ContentType != fileType
+                || file.Size > allowedSize)
+                return Option<string>.None;
+
+            using (var content = new StreamContent(file.OpenReadStream()))
+            {
+                var bytes = await content.ReadAsByteArrayAsync();
+                var text = System.Text.Encoding.UTF8.GetString(bytes);
+
+                return text;
+            }
+        }
+    }
+}
