f2533e362aec329c6660c6d67a47332d5e24405f
Alex <ssaasshha11@yandex.ru>
2023-06-26 13:22:23 +0400

    Add data object for RKK stats

---
+++ AppealManager.Application/Models/RKKStats.cs
@@ -0,0 +1,10 @@
+﻿namespace AppealManager.Application.Models
+{
+    public record struct RKKStats(
+        string MainContractor, 
+        int NumberOfRKK, 
+        int NumOfAppeals)
+    {
+        public int SummaryDocuments { get => NumberOfRKK + NumOfAppeals; }
+    }
+}
