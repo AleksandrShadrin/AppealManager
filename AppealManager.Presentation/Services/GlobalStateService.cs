28fba66ef80d005a17b697e4014c9f3cdbb78751
Alex <ssaasshha11@yandex.ru>
2023-06-27 14:29:03 +0400

    add global state service

---
+++ AppealManager.Presentation/Services/GlobalStateService.cs
@@ -0,0 +1,12 @@
+﻿namespace AppealManager.Presentation.Services
+{
+    public class GlobalStateService
+    {
+        public DateTime ProgramLaunchedAt { get; set; }
+
+        public GlobalStateService(DateTime time)
+        {
+            ProgramLaunchedAt = time;
+        }
+    }
+}
